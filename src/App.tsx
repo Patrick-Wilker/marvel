bc3ec6194c334fddc409fab0536fa1d546b9bfb2
Patrick Wilker Vieira Mota <patrickvieiramota@gmail.com>
2020-09-28 13:00:46 -0300

    Add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import GlobalStyle from './assets/styles/global';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <>
+        <GlobalStyle/>
+        <h1>Hello world</h1>
+    </>
   );
 }
 
