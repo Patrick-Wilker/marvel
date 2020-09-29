1360aec74221ff3b4276945250c0a7568530db70
Patrick Wilker Vieira Mota <patrickvieiramota@gmail.com>
2020-09-29 08:33:14 -0300

    Create the main page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,11 +2,133 @@ import React from 'react';
 
 import Header from '../../components/Header';
 
+import {Search, Results} from './styles';
+
 export default function Home(){
     return(
         <>
             <Header/>
-            <h1>Home</h1>
+            <Search>
+                <form action="">
+                    <div className="input-group">
+                        <label htmlFor="search">Informe o personagem</label>
+                        <input type="text" id="search" placeholder="(Ex: Hulk, Iron Man, Spider-Man, etc)"/>                     
+                    </div>
+                    <div className="button">
+                        <button>
+                            Buscar
+                        </button>
+                    </div>
+                </form>                
+            </Search>
+
+            <Results>
+                <h1>Personagem</h1>
+
+                <div className="character">
+                    <div className="character-img">
+                        <img src="https://upload.wikimedia.org/wikipedia/pt/9/91/Bruce_Banner.jpg" alt="Hulk"/>
+                    </div>
+                    <div className="character-content">
+                        <h2>Hulk</h2>
+                        <p>O Hulk, por vezes referido como O Incrível Hulk é um personagem de quadrinhos/banda desenhada do gênero super-herói, propriedade da Marvel Comics, editora pela qual as histórias do personagem são publicados desde sua criação, nos anos 1960.</p>
+                        <span>Comics | 1583</span>
+
+                        <footer>
+                            Data provided by Marvel &copy; 2020 Marvel
+                        </footer>
+                    </div>
+                </div>
+
+                <h1>Histórias em quadrinhos</h1>
+
+                <ul className="comics">
+                    <li>
+                        <div className="comic-img">
+                            <img src="https://upload.wikimedia.org/wikipedia/pt/9/91/Bruce_Banner.jpg" alt="Hulk"/>
+                        </div>
+                        <div className="comic-content">
+                            <div className="text">
+                               <h2>Titulo</h2>
+                                <p>O Hulk, por vezes referido como O Incrível Hulk é um personagem de quadrinhos/banda desenhada do gênero super-herói, propriedade da Marvel Comics, editora pela qual as histórias do personagem são publicados desde sua criação, nos anos 1960.</p>
+
+                                <span>Criador: Fulano</span> 
+                            </div>
+                            
+                            <footer>
+                                <span>Data provided by Marvel &copy; 2020 Marvel</span>
+                            </footer>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div className="comic-img">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSNW4l9kEi1xGPbK75hvqnrOlOV4S4FB1QiHw&usqp=CAU" alt="Hulk"/>
+                        </div>
+                        <div className="comic-content">
+                            <div className="text">
+                                <h2>Titulo</h2>
+                                <p>O Hulk, por vezes referido como O Incrível Hulk é um personagem de quadrinhos/banda desenhada do gênero super-herói, propriedade da Marvel Comics, editora pela qual as histórias do personagem são publicados desde sua criação, nos anos 1960.</p>
+
+                                <span>Criador: Fulano</span>                                
+                            </div>
+
+                            <footer>
+                                <span>Data provided by Marvel &copy; 2020 Marvel</span>
+                            </footer>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="comic-img">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSieBqtLwWlPpJ-quR2d8uGM7pEVuLXn3hK7w&usqp=CAU" alt="Hulk"/>
+                        </div>
+                        <div className="comic-content">
+                            <div className="text">
+                                <h2>Titulo</h2>
+                                <p>O Hulk, por vezes referido como O Incrível Hulk é um personagem de quadrinhos/banda desenhada do gênero super-herói, propriedade da Marvel Comics, editora pela qual as histórias do personagem são publicados desde sua criação, nos anos 1960.</p>
+
+                                <span>Criador: Fulano</span>                                
+                            </div>
+                            <footer>
+                                <span>Data provided by Marvel &copy; 2020 Marvel</span>
+                            </footer>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="comic-img">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS4LrEf2wkGsn_650oIxMcUE-_p3iD9hP70iQ&usqp=CAU" alt="Hulk"/>
+                        </div>
+                        <div className="comic-content">
+                            <div className="text">
+                                <h2>Titulo</h2>
+                                <p>O Hulk, por vezes referido como O Incrível Hulk é um personagem de quadrinhos/banda desenhada do gênero super-herói, propriedade da Marvel Comics, editora pela qual as histórias do personagem são publicados desde sua criação, nos anos 1960.</p>
+
+                                <span>Criador: Fulano</span>                                
+                            </div>
+                            <footer>
+                                <span>Data provided by Marvel &copy; 2020 Marvel</span>
+                            </footer>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="comic-img">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRdX5GVikzwxXaXaZBzwNO6FjiqaDxMLhKSbg&usqp=CAU" alt="Hulk"/>
+                        </div>
+                        <div className="comic-content">
+                            <div className="text">
+                                <h2>Titulo</h2>
+                                <p>O Hulk, por vezes referido como O Incrível Hulk é um personagem de quadrinhos/banda desenhada do gênero super-herói, propriedade da Marvel Comics, editora pela qual as histórias do personagem são publicados desde sua criação, nos anos 1960.</p>
+
+                                <span>Criador: Fulano</span>                                
+                            </div>
+                            <footer>
+                                <span>Data provided by Marvel &copy; 2020 Marvel</span>
+                            </footer>
+                        </div>
+                    </li>
+
+                </ul>
+            </Results>
         </>
     );
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,226 @@
+import styled from 'styled-components';
+
+export const Search = styled.div`
+    background: #333333;
+    padding: 10px 0;
+    display: flex;
+    justify-content: center;
+
+    form{
+        width: 70%;
+
+        .input-group{
+            label{
+                display: block;
+                margin: 0 0 10px;
+                font-size: 1.5rem;
+                color: #FFF;
+            }
+
+            input{
+                font-size: 1.2rem;
+                display: block;
+                width: 100%;
+                padding: 5px 8px;
+                margin: 0 0 10px;
+            }
+        }
+
+        .button{
+            width: 100%;
+            text-align: right;
+            button{
+                padding: 8px 25px;
+                border: 0;
+                border-radius: 0;
+                color: #FFF;
+                font-weight: bold;
+                background: #EE171F;
+
+                &:hover{
+                    background: #fc383f;
+                }
+            }
+        }
+    }
+    
+    @media only screen and (max-width: 800px){
+        form{
+            width: 90%;
+            .input-group{
+                label{
+                    font-size: 1rem;
+                }
+                input{
+                    font-size: 1rem;
+                }
+            }
+        }
+    }
+    
+`;
+
+export const Results = styled.div`
+
+    h1{
+        width: 70%;
+        margin: 50px auto 20px;
+    }
+
+    .character{
+        width: 70%;
+        margin: 0 auto 50px;
+        display: flex;
+
+        .character-img{
+            width: 35%;
+            height: 350px;
+            background: #ccc;
+            overflow: hidden;
+
+            img{
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+                object-position: center;
+                transition: 1s;
+
+                &:hover{
+                    transform: scale(1.1);
+                }
+            }
+        }
+
+        .character-content{
+            background: #eee;
+            width: 65%;
+            padding: 20px;
+            position: relative;
+
+            h2{
+                text-align: center;
+                margin: 0 0 5px;
+            }
+
+            p{
+                font-weight: bold;
+            }
+
+            span{
+                display: block;
+                margin: 10px 0 40px;
+            }
+
+            footer{
+                width: 100%;
+                position: absolute;
+                margin-left: -20px;
+                margin-bottom: 0;
+                bottom: 0;
+                text-align: center;
+                padding: 8px 0;
+                background: #d6d6d6;
+                border-top: 1px solid #bbb;
+            }
+
+        }
+    }
+
+    .comics{
+        width: 70%;
+        margin: 0 auto 50px;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        gap: 10px;
+
+        li{
+            .comic-img{
+                background: #ccc;
+                height: 300px;
+                overflow: hidden;
+
+                img{
+                    width: 100%;
+                    height: 100%;
+                    object-fit: cover;
+                    object-position: center;
+                    transition: 1s;
+
+                    &:hover{
+                        transform: scale(1.1);
+                    }
+                }
+            }
+
+            .comic-content{
+                background: #eee;
+                position: relative;
+
+                .text{
+                    padding: 20px;
+
+                    h2{
+                        text-align: center;
+                        margin: 0 0 5px;
+                    }
+
+                    p{
+                        font-weight: bold;
+                    }
+
+                    span{
+                        display: block;
+                        margin: 10px 0 40px;
+                    }
+                }
+
+                footer{
+                    margin-top: 20px;
+                    background: #d6d6d6;
+                    border-top: 1px solid #bbb;
+                    position: absolute;
+                    bottom: 0;
+                    width: 100%;
+                    padding: 10px 0;
+                    text-align: center;
+                }
+            }            
+        }
+    }
+
+    @media only screen and (max-width: 1000px){
+        h1{
+            width: 90%;
+        }
+        .character{
+            width: 90%;
+        }
+        .comics{
+            width: 90%;
+        }
+    }
+
+    @media only screen and (max-width: 900px){
+        .comics{
+            grid-template-columns: 1fr 1fr;
+        }
+    }
+
+    @media only screen and (max-width: 600px){
+        .character{
+            display: block;
+
+            .character-img{
+                width: 100%;
+                height: 350px;
+            }
+
+            .character-content{
+                width: 100%;
+            }
+        }
+        .comics{
+            grid-template-columns: 1fr;
+        }
+    }
+`;
