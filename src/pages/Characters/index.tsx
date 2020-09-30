71bfbc981a68c47d9fd31443883f8707763c06ea
Patrick Wilker Vieira Mota <patrickvieiramota@gmail.com>
2020-09-30 00:15:01 -0300

    Create list page of marvel characters

--- src/pages/Characters/index.tsx
+++ src/pages/Characters/index.tsx
@@ -1,12 +1,93 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
+import { ts, hash } from '../../services/marvelConfig';
 
 import Header from '../../components/Header';
 
-export default function Characters(){
-    return(
+import { Container } from './styles';
+
+export default function Characters() {
+
+    const [loading, setLoading] = useState(false);
+    const [loadingMore, setLoadingMore] = useState(false);
+    const [characters, setCharacters] = useState([{
+        id: 0, name: '', description: '', thumbnail: { path: '', extension: '' }, comics: { available: 0 }
+    }]);
+
+    useEffect(() => {
+        async function load() {
+            setLoading(true);
+            const response = await api.get(`/characters?ts=${ts}&apikey=${process.env.REACT_APP_API_KEY}&hash=${hash}`);
+
+            setCharacters(response.data.data.results);
+            setLoading(false);
+        }
+
+        load();
+    }, [])
+
+    async function loadMore() {
+        setLoadingMore(true);
+
+        const offset = characters.length;
+
+        const response = await api.get(`/characters?offset=${offset}&ts=${ts}&apikey=${process.env.REACT_APP_API_KEY}&hash=${hash}`);
+
+        const newCharacters = ([...characters, ...response.data.data.results]);
+
+        setCharacters(newCharacters)
+
+        setLoadingMore(false);
+    }
+
+    return (
         <>
-            <Header/>
-            <h1>Characters</h1>
+            <Header />
+            <Container>
+
+                {
+                    loading ? <h1>Loading...</h1> : (
+                        <ul className="characters">
+
+                            {
+                                characters.map((character, i) => {
+                                    return (<li key={character.id}>
+                                        <div className="character-img">
+                                            <img src={character.thumbnail.path + '.' + character.thumbnail.extension} alt={character.name} />
+                                        </div>
+                                        <div className="character-content">
+                                            <div className="text">
+                                                <h2>{character.name}</h2>
+                                                <p>{character.description ? character.description : 'No description available'}</p>
+
+                                                <span>
+                                                    Comics | {character.comics.available}
+                                                </span>
+
+                                            </div>
+
+                                            <footer>
+                                                <span>Data provided by Marvel &copy; 2020 Marvel</span>
+                                            </footer>
+                                        </div>
+                                    </li>)
+                                })
+                            }
+
+                        </ul>
+                    )
+                }
+
+
+                {
+                    loading ? '' : <div className="button">
+                        <button onClick={loadMore}>
+                            {loadingMore ? 'Loading' : 'More'}
+                        </button>
+                    </div>
+                }
+
+            </Container>
         </>
     );
 }
---
+++ src/pages/Characters/styles.ts
@@ -0,0 +1,101 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 70%;
+    margin: 0 auto;
+
+    h1{
+        margin-top: 50px;
+        text-align: center;
+    }
+
+    ul{
+        margin: 50px 0;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: 10px;
+
+        li{
+            .character-img{
+                background: #ccc;
+                height: 400px;
+                overflow: hidden;
+
+                img{
+                    width: 100%;
+                    height: 100%;
+                    object-fit: cover;
+                    object-position: center;
+                    transition: 1s;
+
+                    &:hover{
+                        transform: scale(1.1);
+                    }
+                }
+            }
+
+            .character-content{
+                background: #eee;
+                position: relative;
+
+                .text{
+                    padding: 20px;
+
+                    h2{
+                        text-align: center;
+                        margin: 0 0 5px;
+                    }
+
+                    p{
+                        font-weight: bold;
+                    }
+
+                    span{
+                        display: block;
+                        margin: 10px 0 40px;
+                    }
+                }
+
+                footer{
+                    margin-top: 20px;
+                    background: #d6d6d6;
+                    border-top: 1px solid #bbb;
+                    position: absolute;
+                    bottom: 0;
+                    width: 100%;
+                    padding: 10px 0;
+                    text-align: center;
+                }
+            }   
+        }
+    }
+
+    .button{
+        width: 100%;
+        text-align: center;
+        margin-bottom: 50px;
+        button{
+            padding: 8px 25px;
+            border: 0;
+            border-radius: 0;
+            color: #FFF;
+            font-weight: bold;
+            background: #EE171F;
+
+            &:hover{
+                background: #fc383f;
+            }
+        }
+    }
+
+
+    @media only screen and (max-width: 1000px){
+        width: 90%;
+    }
+
+    @media only screen and (max-width: 750px){
+        ul{
+            grid-template-columns: 1fr;
+        }
+    }
+`;
